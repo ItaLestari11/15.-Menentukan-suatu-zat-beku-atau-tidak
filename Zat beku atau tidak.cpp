e39aa2763c721387d5ec1f47e3752659a9645f68
ItaLestari11 <45660967+ItaLestari11@users.noreply.github.com>
2018-12-15 16:33:27 +0700

    Add files via upload

---
+++ Zat beku atau tidak.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a;
+
+cout<<"Program menentukan zat beku atau cair"<<endl;
+cout<<"ITA LESTARI"<<endl;
+cout<<"311810183"<<endl;
+cout<<"Masukan nilai suhu"<<endl;
+cin>>a;
+
+if(a>0)
+    cout<<"Maka zat tersebut dalam keadaan cair:";
+    else
+        cout<<"Maka zat tersebut dalam keadaan beku:";
+    return 0;
+}
